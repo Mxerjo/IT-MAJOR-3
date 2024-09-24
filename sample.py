2c7bb2de42fb901e95433c19aef010e29275817e
Marjore <jamodiongmarjore@gmail.com>
2024-09-24 17:35:23 +0800

    SQL ALCHEMY

--- sample.py
+++ sample.py
@@ -91,9 +91,55 @@ def create_department(format: DeparmentBase, db: SessionLocal = Depends(get_db))
     db.refresh(department)
     return department
 
-@app.get("/api/department", response_model=list[DeparmentBase])
-def create_department(format: DeparmentBase, db: SessionLocal = Depends(get_db)):
-    department = Department(name = format.name,
-                            description = format.description)
-    db = db.query(Department).filter(Department.is_active == True).all() 
+# @app.get("/api/department", response_model=list[DeparmentBase])
+# def create_department(format: DeparmentBase, db: SessionLocal = Depends(get_db)):
+#     department = Department(name = format.name,
+#                             description = format.description)
+#     db = db.query(Department).filter(Department.is_active == True).all() 
+#     return department
+
+# Specific ID
+@app.get("/api/department/{id}", response_model=[DepartmentResponse])
+def get_department(id: str, db: SessionLocal = Depends(get_db)):
+    department = db.query(Department).filter(Department.id == id).first()
+    if department is None :
+        raise HTTPException(status_code=404, detail="Department not found")
+    else :
+        return department
+    
+@app.put("/api/department/{id}", response_model=[DepartmentResponse])
+def edit_department(id: str, db: SessionLocal = Depends(get_db)):
+    department = db.query(Department).filter(Department.id == id).first()
+    department.name = format.name
+    department.description = format.description
+    db.commit()
+    db.refresh(department)
     return department
+    
+
+@app.delete("/api/department/{id}", response_model=[DepartmentResponse])
+def deactivate_department(id: str, db: SessionLocal = Depends(get_db)):
+    department = db.query(Department).filter(Department.id == id).first()
+    department.is_active = False
+    db.commit()
+    db.refresh(department)
+    return department
+
+@app.post("/api/students", response_model = StudentResponse)
+def create_students(format: StudentBase, db: SessionLocal = Depends(get_db)):
+    student = Student(firstname = format.firstname,
+                      lastname = format.lastname,
+                      level = format.level,
+                      department_id = format.department_id)
+    db.add(student)
+    db.commit()
+    db.refresh(student)
+    return student
+
+@app.get("/api/department/students/{id}", response_model = StudentResponse)
+def get_department_student(id: str, db: SessionLocal = Depends(get_db)):
+    department = db.query(Department).filter(Department.id == id).first()
+    db.commit()
+    db.refresh(department)
+    return department.students
+
